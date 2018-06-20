aeca1cf35b7302d650d54d08b2cb42c3f538329d
Benjamin Hills <bhills@gmail.com>
2018-06-20 18:45:28 +0000

    updated server to use ssl authentication

--- irca_flask_serv/__init__.py
+++ irca_flask_serv/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_path='/data/instance')
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'irca_flask_serv.sqlite'),
